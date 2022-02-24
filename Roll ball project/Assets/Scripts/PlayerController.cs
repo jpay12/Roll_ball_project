413e1ea588bb97e9acee1f652dedf6d47a070af9
jpay12 <97638950+jpay12@users.noreply.github.com>
2022-02-23 21:09:40 -0500

    added walls
    added walls to the level

--- Roll ball project/Assets/Scripts/PlayerController.cs
+++ Roll ball project/Assets/Scripts/PlayerController.cs
@@ -26,10 +26,35 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y; 
     }
 
+    private void Update()
+    {
+      //when the jump buttonis pressed
+      if( Input.GetButtonDown("Jump") )
+        
+      //(todo/should do: check to see if the player is on the ground)
+      //add an upward force to the player 
+      rb.AddForce(new Vector3(0f,300f,0f));  
+
+    }
+
+
     void FixedUpdate() 
     {
+        // Set some local float variables equal to the value of our Horizontal and Vertical Inputs 
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Verical"); 
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical); 
+
         Vector3 movement = new Vector3( movementX, 0.0f, movementY); 
         rb.AddForce(movement * speed); 
 
     }
+
+
+
+
+
+
+
 }
